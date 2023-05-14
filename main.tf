393fa19ce63d1d0e145b51252177621574dc00c2
sagar <sagar@gmail.com>
2023-05-14 20:12:57 +0530

    commit16

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my-insta" {
   ami           = "ami-08333bccc35d71140"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  key_name = "ohio-new"
+  key_name = "sagar.pem"
   security_groups = ["sg-00c07de1ba6fa6eaf"]
   subnet_id = "subnet-00f8785a30c977f96"
 

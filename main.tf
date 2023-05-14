2c43535f2e37c7364338a988c96d4e894eeb5ded
sagar <sagar@gmail.com>
2023-05-14 20:43:47 +0530

    commit18

--- main.tf
+++ main.tf
@@ -6,8 +6,9 @@ resource "aws_instance" "my-insta" {
   ami           = "ami-08333bccc35d71140"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  key_name = "ohio-new"
-  security_groups = ["defaults"]
+  key_name = "sagar"
+  security_groups = ["sg-00c07de1ba6fa6eaf"]
+  subnet_id = "subnet-00f8785a30c977f96"
 
   ebs_block_device {
     device_name = "/dev/sdf"

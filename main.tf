7e406b2349bed66c5d3848b1262c8c0f146a3f1d
sagar <sagar@gmail.com>
2023-05-14 21:50:11 +0530

    commit20

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "this" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
    
   }
 

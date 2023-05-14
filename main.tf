3913d78f424b429eb181f1a4d982a8eb386d5187
sagar <sagar@gmail.com>
2023-05-14 21:29:45 +0530

    commit19

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_instance" "my-insta" {
   ami           = "ami-08333bccc35d71140"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  key_name = "sagar"
-  security_groups = ["sg-00c07de1ba6fa6eaf"]
-  subnet_id = "subnet-00f8785a30c977f96"
+  key_name = "tom-new"
+  security_groups = ["default"]
+  vpc_security_group_ids = [aws_security_group.this.id]
 
-  ebs_block_device {
+  root_block_device {
     device_name = "/dev/sdf"
     volume_size = 10
     volume_type = "gp2"
@@ -20,3 +20,30 @@ resource "aws_instance" "my-insta" {
     Name = "tf-demo-instance"
   }
 }
+
+
+resource "aws_security_group" "this" {
+  name        = "demo-sg"
+  vpc_id      = "vpc-088a111ca5a26a107" 
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+   
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+
+  tags = {
+    Name = "my-demo-tf-sg"
+  }
+}   

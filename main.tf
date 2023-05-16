97fe25c6852787c8d26a5b7b7e534ccd7d80b1e1
sagar <sagar@gmail.com>
2023-05-16 22:24:38 +0530

    commit27

--- main.tf
+++ main.tf
@@ -11,15 +11,7 @@ resource "aws_instance" "my-insta" {
   vpc_security_group_ids = [aws_security_group.this.id]
   subnet_id = "subnet-00f8785a30c977f96"
     
-    user_data = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo touch /var/www/html/index.html
-sudo echo "welcome to hello world $(hostname)" > /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
-EOF
+    user_data =  "${file("install_script.sh")}"
 
   root_block_device {
     volume_size = 10
